78659ce151cca159cf63bcfa4a961fa6727a31a5
aaugustoag <49174397+aaugustoag@users.noreply.github.com>
2019-04-07 19:56:11 -0300

    Add files via upload

---
+++ babyshark.py
@@ -0,0 +1,7 @@
+baby_shark = ["Baby", "Daddy", "Mommy", "Grandpa", "Grandma"]
+
+
+for item in baby_shark:
+    for i in range(3):
+        print(item+" shark "+"doo "*6)
+    print (item+" shark!")
