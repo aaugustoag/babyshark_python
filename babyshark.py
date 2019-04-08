2bb1e7c1c4127b96c41336ac242db088c6e56477
aaugustoag <49174397+aaugustoag@users.noreply.github.com>
2019-04-08 11:49:26 -0300

    Update babyshark.py

--- babyshark.py
+++ babyshark.py
@@ -1,5 +1,4 @@
 baby_shark = ["Baby", "Daddy", "Mommy", "Grandpa", "Grandma"]
 
 for item in baby_shark:
-    print((item+" shark "+"doo "*6)*3)
-    print (item+" shark!")
+    print((item+" shark "+"doo "*6+"\n")*3+item+" shark!")
